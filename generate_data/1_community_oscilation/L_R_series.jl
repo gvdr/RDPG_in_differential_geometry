b62762b1381b25bc208af0ebf75ef362465c58da
StirlingSmith <stirlingjamessmith@gmail.com>
2025-07-24 13:01:57 +1200

    1 community network creating scripts (oscillate in and out of community)

---
+++ generate_data/1_community_oscilation/L_R_series.jl
@@ -0,0 +1,21 @@
+function L_R_series(d, tsteps)
+    L_series= []
+    R_series = []
+
+    function svd_decompose(A,d)
+        L,Σ,R = svds(A; nsv=d, v0=[Float64(i%7) for i in 1:minimum(size(A))])[1]
+        L̂ = L * diagm(.√Σ)
+        R̂ = R * diagm(.√Σ)
+        return (L̂ = L̂, R̂ = R̂)
+    end
+
+    for i in 1:tsteps
+        if i != 1
+            L, R = svd_decompose(series[i], convert(Int, d))
+        end
+        push!(L_series, L)
+        push!(R_series, R)
+
+    end
+    return L_series, R_series
+end
---
+++ generate_data/1_community_oscilation/create_series.jl
@@ -0,0 +1,12 @@
+function create_series(n::Int, tsteps::Int)
+    A = Matrix(BlockDiagonal([zeros(1,1), ones(n,n)]))
+    series = [copy(A)]
+
+    for i in 2:tsteps
+        A[1, (i%n)+2] = (A[1, (i%n)+2]+1)%2
+        A[(i%n)+2, 1] = (A[(i%n)+2, 1]+1)%2
+        push!(series, copy(A)) 
+    end
+
+    return series
+end
